7948e82f795c9d1ee43714863c373a44b6aba44b
a1rlay <a1rlay@outlook.com>
2025-12-14 20:44:34 -0700

    Remove some useless code in monokai

--- lua/custom/themes/monokai.lua
+++ lua/custom/themes/monokai.lua
@@ -5,35 +5,7 @@ return {
     require('monokai-pro').setup {
       terminal_colors = true,
       filter = 'pro',
-      background_clear = {
-        'float_win',
-        'toggleterm',
-        'telescope',
-        'which-key',
-        'renamer',
-        'notify',
-        'nvim-tree',
-        'neo-tree',
-        'bufferline',
-        'statusline',
-        'signcolumn',
-        'line_nr',
-        'end_of_buffer',
-      },
+      transparent_background = false,
     }
-
-    local function clear_bg(group)
-      pcall(vim.api.nvim_set_hl, 0, group, { bg = 'none', ctermbg = 'none' })
-    end
-
-    for _, g in ipairs {
-      'SignColumn',
-      'SignColumnSB',
-      'LineNr',
-      'CursorLineNr',
-      'FoldColumn',
-    } do
-      clear_bg(g)
-    end
   end,
 }
